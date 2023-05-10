1ae3656f4d6f09db2ef88001d015ab6892706619
PufSunCat <114050978+PufSunCat@users.noreply.github.com>
2023-05-10 09:43:52 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+
+const coolButtom = document.querySelector(".js-cool");
+coolButtom.addEventListener('click', () =>{
+    window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley')
+})
